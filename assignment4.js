8a2bb8dfd985b53b054b5da1e3923d45385f0228
Fatima-Rasheed <fatimarasheedch@gmail.com>
2023-08-24 15:31:03 +0500

    assignment4

--- assignment4.js
+++ assignment4.js
@@ -2,8 +2,15 @@
 // Question no 1
 function array1(array, index, value) {
     array.splice(index, 0, value);
-    //    return array;
-    console.log(array);
+    return array;
 }
 var myArray = [1, 2, 3, 4, 5];
 console.log(array1(myArray, 1, 6));
+//    Question no 2
+var cart = [{ name: "bag", quantity: 2, price: 5000 }];
+function additems(name1, quantity1, price1) {
+    cart.splice(0, 0, { name: name1, quantity: quantity1, price: price1 });
+    console.log(cart);
+}
+additems("lehnga", 1, 20000);
+additems("shoes", 10, 90000);
--- assignment4.ts
+++ assignment4.ts
@@ -1,9 +1,18 @@
 // Question no 1
 function array1(array: any[], index: number, value: number) {
     array.splice(index, 0, value);
-//    return array;
-console.log (array);
+       return array;
 
   }
  var myArray: number[] = [1, 2, 3, 4, 5];
    console.log(array1(myArray, 1, 6));
+
+//    Question no 2
+var cart: any[]=[{name:"bag", quantity:2 ,price:5000}];
+function additems(name1:string,quantity1:number,price1:number)
+{
+cart.splice(0 ,0,{name:name1,quantity: quantity1,price:price1})
+console.log(cart)
+}
+additems("lehnga",1,20000)
+additems("shoes",10,90000)
