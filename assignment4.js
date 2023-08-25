05d19973a919e1bfd3931eaba7d22b110bfdd82b
Fatima-Rasheed <fatimarasheedch@gmail.com>
2023-08-25 13:09:26 +0500

    assignment4

--- assignment4.js
+++ assignment4.js
@@ -6,59 +6,59 @@ function array1(array, index, value) {
 }
 var myArray = [1, 2, 3, 4, 5];
 console.log(array1(myArray, 1, 6));
-//    Question no 2
-var cart = ["bags", "shoes", "glasses", "jewllery"];
-// AddItems
-function addItems() {
-    cart.splice(3, 0, "Mangoes");
-    return cart;
-}
-console.log();
-console.log(addItems());
-// REmoveItems
-function removeItems() {
-    cart.splice(2, 1);
-    return cart;
-}
-console.log(cart);
-console.log(removeItems());
-// Update items
-function updateItems() {
-    cart.splice(4, 0, "perfumes");
-    return cart;
-}
-console.log(cart);
-console.log(updateItems());
-// Question no 3
-var i = 1;
-while (i <= 25) {
-    console.log(i);
-    i++;
-}
-// Question no 4
-var i = 1;
-while (i <= 20) {
-    if (i % 2 == 0) {
-        console.log(i);
-    }
-    i++;
-}
-// Question no 5
-function factorial(n) {
-    var num = 1;
-    while (n > 0) {
-        num *= n;
-        console.log(num);
-    }
-}
-factorial(3);
-// Question no 6
-var num = [20, 54, -74, 83, -35, 76, -44];
-while (i > 0) {
-    console.log(i);
-}
-i++;
-if (num[i] < 0) {
-    num.pop();
-    console.log(num);
-}
+// //    Question no 2
+// var cart:string[]=["bags","shoes","glasses","jewllery"]
+// // AddItems
+// function addItems(){
+//     cart.splice(3,0,"Mangoes")
+//     return cart;
+// }
+// console.log()
+// console.log(addItems())
+// // REmoveItems
+// function removeItems(){
+//     cart.splice(2,1)
+//     return cart;
+// }
+// console.log(cart)
+// console.log(removeItems())
+// // Update items
+// function updateItems(){
+//     cart.splice(4,0,"perfumes")
+//     return cart;
+// }
+// console.log(cart)
+// console.log(updateItems())
+// // Question no 3
+// var i:number=1;
+// while (i<=25){
+// console.log(i)
+// i++;
+// }
+// // Question no 4
+// var i :number=1;
+// while(i<=20){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+//     i++;
+// }
+// // Question no 5
+// function factorial(n:number){
+// var num:number=1;
+// while(n>0){
+//     num*=n;
+//      console.log(num)
+//  }
+// }
+// factorial(3)
+// // Question no 6
+// var num:number[]=[20,54,-74,83,-35,76,-44];
+// while(i>0){
+//     console.log(i);
+// }
+// i++;
+// if(num[i]<0){
+//     num.pop();
+//     console.log(num)
+// }
