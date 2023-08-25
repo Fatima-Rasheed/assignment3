d867f805639cf08c09a10f829aa6f50c5063b18d
Fatima-Rasheed <fatimarasheedch@gmail.com>
2023-08-25 13:37:28 +0500

    assignment4

--- assignment4.js
+++ assignment4.js
@@ -44,21 +44,21 @@ while (i <= 20) {
     i++;
 }
 // Question no 5
-function factorial(n) {
-    var num = 1;
-    while (n > 0) {
-        num *= n;
-        console.log(num);
-    }
-}
-factorial(3);
+// function factorial(n:number){
+// var num:number=1;
+// while(n>0){
+//     num*=n;
+//      console.log(num)
+//  }
+// }
+// factorial(3)
 // Question no 6
-// var num:number[]=[20,54,-74,83,-35,76,-44];
-// while(i<num.length){
-// }
-// i++;
-// if(num[i]<0){
-//     num.splice(i,1);
-//     console.log(num);
-// }
+var num = [20, 54, -74, 83, -35, 76, -44];
+while (i < num.length) {
+}
+i++;
+if (num[i] < 0) {
+    num.splice(i, 1);
+    console.log(num);
+}
 // Question no 7
