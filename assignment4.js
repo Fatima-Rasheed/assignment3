48dc3ab76274e23d38bf46279842c829a54f8b0c
Fatima-Rasheed <fatimarasheedch@gmail.com>
2023-08-31 15:28:08 +0500

    assignment4

--- assignment4.js
+++ assignment4.js
@@ -4,8 +4,10 @@ function array1(array, index, value) {
     array.splice(index, 0, value);
     return array;
 }
+let value = 22;
+let index = 2;
 var myArray = [1, 2, 3, 4, 5];
-console.log(array1(myArray, 1, 6));
+console.log(array1(myArray, index, value));
 //    Question no 2
 var cart = ["bags", "shoes", "glasses", "jewllery"];
 // AddItems
@@ -32,35 +34,36 @@ console.log(updateItems());
 // Question no 3
 var i = 1;
 while (i <= 25) {
-    console.log(i);
+    console.log("First 25 integers", i);
     i++;
 }
 // Question no 4
 var i = 1;
 while (i <= 20) {
     if (i % 2 == 0) {
-        console.log(i);
+        console.log("First 10 even numbers=", i);
     }
     i++;
 }
 // Question no 5
-// function factorial(n:number){
-// var num:number=1;
-// while(n>0){
-//     num*=n;
-//      console.log(num)
-//  }
-// i++;
-// }
-// factorial(3)
+function factorial(n) {
+    var num = 1;
+    while (n >= 1) {
+        num *= n;
+        n--;
+    }
+    return num;
+}
+let n = 2;
+console.log("The facatorial is=", factorial(n));
 // Question no 6
-var num = [20, 54, -74, 83, -35, 76, -44];
-while (i < num.length) {
+var num1 = [20, 54, -74, 83, -35, 76, -44];
+while (i < num1.length) {
 }
 i++;
-if (num[i] < 0) {
-    num.splice(i, 1);
-    console.log(num);
+if (num1[i] < 0) {
+    num1.splice(i, 1);
+    console.log(num1);
 }
 // Question no 7
 function sumArray(arraytosum) {
@@ -73,4 +76,16 @@ function sumArray(arraytosum) {
     return sum;
 }
 var newArray = [39, 76, 22, 54];
-console.log(sumArray(newArray));
+console.log("sum of array is=", sumArray(newArray));
+// Question no 8
+function temperature(celcius) {
+    let k = 0;
+    let F = [];
+    while (k < celcius.length) {
+        F[k] = ((celcius[k] * 9 / 5) + 32);
+        i++;
+    }
+    return F;
+}
+let celcius = [44, 76, 54, 33, 98];
+console.log("Temperature is converted to F" + temperature(celcius));
--- assignment4.ts
+++ assignment4.ts
@@ -4,8 +4,10 @@ function array1(array: any[], index: number, value: number) {
        return array;
 
   }
+  let value=22;
+  let index=2;
  var myArray: number[] = [1, 2, 3, 4, 5];
-   console.log(array1(myArray, 1, 6));
+   console.log(array1(myArray,index,value));
 
 //    Question no 2
 var cart:string[]=["bags","shoes","glasses","jewllery"]
@@ -33,38 +35,40 @@ console.log(updateItems())
 // Question no 3
 var i:number=1;
 while (i<=25){
-console.log(i)
+console.log("First 25 integers",i)
 i++;
 }
 // Question no 4
 var i :number=1;
 while(i<=20){
     if(i%2==0){
-        console.log(i)
+        console.log("First 10 even numbers=",i)
     }
     i++;
 }
 // Question no 5
-// function factorial(n:number){
-// var num:number=1;
-// while(n>0){
-//     num*=n;
-//      console.log(num)
-//  }
-// i++;
-// }
-// factorial(3)
+function factorial(n:number):number{
+var num:number=1;
+while(n>=1){
+    num*=n;
+     n--;
+}
+     return num;
+ }
+let n=2;
+console.log ("The facatorial is=",factorial(n));
 // Question no 6
-var num:number[]=[20,54,-74,83,-35,76,-44];
-while(i<num.length){
+var num1:number[]=[20,54,-74,83,-35,76,-44];
+while(i<num1.length){
    
 }
 i++;
-if(num[i]<0){
-    num.splice(i,1);
-    console.log(num);
-}
 
+
+if(num1[i]<0){
+    num1.splice(i,1);
+    console.log(num1);
+}
 // Question no 7
 function sumArray(arraytosum:number[]){
 var sum:number=0;
@@ -76,5 +80,17 @@ while(j<arraytosum.length){
 return sum;
 }
 var newArray:number[]=[39,76,22,54]
-console.log(sumArray(newArray))
+console.log("sum of array is=",sumArray(newArray))
+// Question no 8
+function temperature(celcius:number[]){
+    let k=0;
+    let F:number[]=[];
+    while(k<celcius.length){
+        F[k]=((celcius[k]*9/5)+32);
+        i++;
+    }
+    return F;
+    }
+let celcius:number[]=[44,76,54,33,98];
+console.log("Temperature is converted to F"+temperature(celcius));
 
